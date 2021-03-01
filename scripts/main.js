969a9fb8bca5d0c38cc937602255eae1bacfec0f
Asadullo2502 <asadullosadirdinov@gmail.com>
2021-03-01 12:26:15 +0500

    birinchi qadam

---
+++ scripts/main.js
@@ -0,0 +1,575 @@
+let savollar = [{
+        id: 1,
+        savol: `Shlagbaumli temir yo'l kesishmasi`,
+        javob: `../images/1.gif`
+    }, {
+        id: 2,
+        savol: `Shlagbaumsiz temir yo’l kesishmasi`,
+        javob: `../images/2.gif`
+    }, {
+        id: 3,
+        savol: `Diqqat "UZP"`,
+        javob: `../images/3.gif`
+    }, {
+        id: 4,
+        savol: `Bir izli temir yo’l`,
+        javob: `../images/4.gif`
+    }, {
+        id: 5,
+        savol: `Ko’p izli temir yo’l`,
+        javob: `../images/5.gif`
+    }, {
+        id: 6,
+        savol: `3 Temir yo’l kesishmasining yaqinligi haqida ogohlantirish`,
+        javob: `../images/6.gif`
+    },
+
+    {
+        id: 7,
+        savol: `2 Temir yo’l kesishmasining yaqinligi haqida ogohlantirish`,
+        javob: `../images/7.gif`
+    }, {
+        id: 8,
+        savol: `1.4.3 Temir yo’l kesishmasining yaqinligi haqida ogohlantirish`,
+        javob: `../images/8.gif`
+    }, {
+        id: 9,
+        savol: `1.4.4 Temir yo’l kesishmasining yaqinligi haqida ogohlantirish`,
+        javob: `../images/9.gif`
+    }, {
+        id: 10,
+        savol: `1.4.5 Temir yo’l kesishmasining yaqinligi haqida ogohlantirish`,
+        javob: `../images/10.gif`
+    },
+    {
+        id: 11,
+        savol: `1.4.6 Temir yo’l kesishmasining yaqinligi haqida ogohlantirish`,
+        javob: `../images/11.gif`
+    }, {
+        id: 12,
+        savol: 'Tramvay yo’li bilan kesishuv',
+        javob: `../images/12.gif`
+    }, {
+        id: 13,
+        savol: 'Teng ahamiyatli yo’llar kesishuvi',
+        javob: `../images/13.gif`
+    }, {
+        id: 14,
+        savol: 'Aylanma harakatlanish bilan kesishuv',
+        javob: `../images/14.gif`
+    }, {
+        id: 15,
+        savol: 'Svetofor tartibga soladi',
+        javob: `../images/15.gif`
+    }, {
+        id: 16,
+        savol: 'Ko’tarma ko’prik',
+        javob: `../images/16.gif`
+    },
+
+    {
+        id: 17,
+        savol: 'Sohilga chiqish',
+        javob: `../images/17.gif`
+    }, {
+        id: 18,
+        savol: 'Xavfli burilish o`nga',
+        javob: `../images/18.gif`
+    }, {
+        id: 19,
+        savol: 'Xavfli burilish chapga',
+        javob: `../images/19.gif`
+    }, {
+        id: 20,
+        savol: 'Xavfli burilishlar o`nga',
+        javob: `../images/20.gif`
+    },
+    {
+        id: 21,
+        savol: 'Xavfli burilishlar chapga',
+        javob: `../images/21.gif`
+    }, {
+        id: 22,
+        savol: 'Tik nishablik',
+        javob: `../images/22.gif`
+    }, {
+        id: 23,
+        savol: 'Tik balandlik',
+        javob: `../images/23.gif`
+    }, {
+        id: 24,
+        savol: 'Sirpanchiq yo’l',
+        javob: `../images/24.gif`
+    }, {
+        id: 25,
+        savol: 'Notekis yo’l',
+        javob: `../images/25.gif`
+    }, {
+        id: 26,
+        savol: 'Tosh otilish xavfi',
+        javob: `../images/26.gif`
+    },
+
+    {
+        id: 27,
+        savol: 'Yo’lning torayishi o`rtaga',
+        javob: `../images/27.gif`
+    }, {
+        id: 28,
+        savol: 'Yo’lning torayishi o`nga',
+        javob: `../images/28.gif`
+    }, {
+        id: 29,
+        savol: 'Yo’lning torayishi chapga',
+        javob: `../images/29.gif`
+    }, {
+        id: 30,
+        savol: 'Ikki tomonlama harakatlanish',
+        javob: `../images/30.gif`
+    },
+    {
+        id: 31,
+        savol: 'Piyodalar o’tish joyi',
+        javob: `../images/31.gif`
+    }, {
+        id: 32,
+        savol: 'Bolalar',
+        javob: `../images/32.gif`
+    }, {
+        id: 33,
+        savol: 'Velosiped yo’lkasi bilan kesishuv',
+        javob: `../images/33.gif`
+    }, {
+        id: 34,
+        savol: 'Ta’mirlash ishlari',
+        javob: `../images/34.gif`
+    }, {
+        id: 35,
+        savol: 'Mol haydab o’tish',
+        javob: `../images/35.gif`
+    }, {
+        id: 36,
+        savol: 'Yovvoyi hayvonlar',
+        javob: `../images/36.gif`
+    },
+
+    {
+        id: 37,
+        savol: 'Toshlar tushishie',
+        javob: `../images/37.gif`
+    }, {
+        id: 38,
+        savol: 'Yonlama shamol',
+        javob: `../images/38.gif`
+    }, {
+        id: 39,
+        savol: 'Pastlab uchuvchi samolyotlar',
+        javob: `../images/39.gif`
+    }, {
+        id: 40,
+        savol: 'Tonnel',
+        javob: `../images/40.gif`
+    },
+    {
+        id: 41,
+        savol: 'Boshqa xavf-xatar',
+        javob: `../images/41.gif`
+    }, {
+        id: 42,
+        savol: `Sun'iy yo'l notekisliklari`,
+        javob: `../images/42.png`
+    }, {
+        id: 43,
+        savol: 'Burilishning yo’nalishi o`nga',
+        javob: `../images/43.gif`
+    }, {
+        id: 44,
+        savol: 'Burilishning yo’nalishi chapga',
+        javob: `../images/44.gif`
+    }, {
+        id: 45,
+        savol: 'Burilishning yo’nalishi',
+        javob: `../images/45.gif`
+    }, {
+        id: 46,
+        savol: 'Tirbandlik',
+        javob: `../images/46.gif`
+    },
+
+
+    {
+        id: 47,
+        savol: 'Asosiy yo’l',
+        javob: `../images/47.gif`
+    }, {
+        id: 48,
+        savol: 'Asosiy yo’lning oxiri',
+        javob: `../images/48.gif`
+    }, {
+        id: 49,
+        savol: ' Ikkinchi darajali yo’l bilan kesishuv',
+        javob: `../images/49.gif`
+    }, {
+        id: 50,
+        savol: 'tutashuv o’ngdan',
+        javob: `../images/50.gif`
+    },
+    {
+        id: 51,
+        savol: 'tutashuv chapdan',
+        javob: `../images/51.png`
+    }, {
+        id: 52,
+        savol: 'Yo’l bering',
+        javob: `../images/52.gif`
+    }, {
+        id: 53,
+        savol: 'To’xtamasdan harakatlanish taqiqlangan',
+        javob: `../images/53.gif`
+    }, {
+        id: 54,
+        savol: 'Ro’para harakatlanishning ustunligi',
+        javob: `../images/54.gif`
+    }, {
+        id: 55,
+        savol: 'Ro’paradagi harakatlanishga nisbatan imtiyoz',
+        javob: `../images/55.gif`
+    }, {
+        id: 56,
+        savol: `Kirish ta'qiqlangan`,
+        javob: `../images/56.gif`
+    },
+
+    {
+        id: 57,
+        savol: `Harakatlanish ta'qiqlangan`,
+        javob: `../images/57.gif`
+    }, {
+        id: 58,
+        savol: 'Mexanik transport vositalarining harakatlanishi taqiqlangan',
+        javob: `../images/59.gif`
+    }, {
+        id: 59,
+        savol: 'Yuk avtomobillarining harakatlanishi taqiqlangan',
+        javob: `../images/59.gif`
+    }, {
+        id: 60,
+        savol: 'Mototsikllar harakatlanishi taqiqlangan',
+        javob: `../images/60.gif`
+    },
+    {
+        id: 61,
+        savol: ' Traktorlar harakatlanishi taqiqlangan',
+        javob: `../images/61.gif`
+    }, {
+        id: 62,
+        savol: 'Tirkama bilan harakatlanish taqiqlangan',
+        javob: `../images/62.gif`
+    }, {
+        id: 63,
+        savol: 'Ot-arava harakatlanishi taqiqlangan',
+        javob: `../images/63.gif`
+    }, {
+        id: 64,
+        savol: `Velosipedda harakatlanish ta'qiqlangan`,
+        javob: `../images/64.gif`
+    }, {
+        id: 65,
+        savol: `Piyodalarning harakatlanishi ta'qiqlangan`,
+        javob: `../images/65.gif`
+    }, {
+        id: 66,
+        savol: 'Vazn cheklangan',
+        javob: `../images/66.gif`
+    },
+
+    {
+        id: 67,
+        savol: 'O‘qqa tushadigan og‘irlik cheklangan',
+        javob: `../images/67.gif`
+    }, {
+        id: 68,
+        savol: 'Cheklangan balandlik',
+        javob: `../images/68.gif`
+    }, {
+        id: 69,
+        savol: 'Cheklangan kenglik',
+        javob: `../images/69.gif`
+    }, {
+        id: 70,
+        savol: 'Cheklangan uzunlik',
+        javob: `../images/70.gif`
+    },
+    {
+        id: 71,
+        savol: 'Eng kam oraliq',
+        javob: `../images/71.gif`
+    }, {
+        id: 72,
+        savol: ' Bojxona',
+        javob: `../images/72.gif`
+    }, {
+        id: 73,
+        savol: 'Xavf-xatar',
+        javob: `../images/73.gif`
+    }, {
+        id: 74,
+        savol: `O’ngga burilish ta'qiqlanadi`,
+        javob: `../images/74.gif`
+    }, {
+        id: 75,
+        savol: 'Chapga burilish taqiqlanadi',
+        javob: `../images/75.gif`
+    }, {
+        id: 76,
+        savol: `Qayrilish ta'qiqlanadi`,
+        javob: `../images/76.gif`
+    },
+
+    {
+        id: 77,
+        savol: 'Quvib o’tish taqiqlanadi',
+        javob: `../images/77.gif`
+    }, {
+        id: 78,
+        savol: `Quvib o’tish ta'qiqlangan hududning oxiri`,
+        javob: `../images/80.gif`
+    }, {
+        id: 79,
+        savol: 'Yuk avtomobillarida quvib o’tish taqiqlangan',
+        javob: `../images/79.gif`
+    }, {
+        id: 80,
+        savol: 'Yuk avtommobillarida quvib o’tish taqiqlangan hududning oxiri',
+        javob: `../images/80.gif`
+    },
+    {
+        id: 81,
+        savol: 'Yuqori tezlik cheklangan',
+        javob: `../images/81.gif`
+    }, {
+        id: 82,
+        savol: 'Yuqori tezlik cheklangan hududning oxiri',
+        javob: `../images/82.gif`
+    }, {
+        id: 83,
+        savol: `Tovuch moslamalaridan foydalanish ta'qiqlangan`,
+        javob: `../images/83.gif`
+    }, {
+        id: 84,
+        savol: 'To’xtash taqiqlangan',
+        javob: `../images/84.gif`
+    }, {
+        id: 85,
+        savol: `To’xtab turish ta'qiqlangan`,
+        javob: `../images/85.gif`
+    }, {
+        id: 86,
+        savol: 'Oyning toq kunlarida to’xtab turish taqiqlanadi',
+        javob: `../images/86.gif`
+    },
+
+    {
+        id: 87,
+        savol: 'Oyning juft kunlarida to’xtab turish taqiqlanadi',
+        javob: `../images/87.gif`
+    }, {
+        id: 88,
+        savol: 'Barcha cheklovlarning oxiri',
+        javob: `../images/88.gif`
+    }, {
+        id: 89,
+        savol: `Xavfli yuklarga ega transport vositalarining harakati ta'qiqlanadi`,
+        javob: `../images/89.gif`
+    }, {
+        id: 90,
+        savol: `Portlovchi va yonuvchan yuklarga ega transport vositalarining harakatlanishi ta'qiqlanadi`,
+        javob: `../images/90.gif`
+    },
+    {
+        id: 91,
+        savol: 'Harakatlanish to’g’riga',
+        javob: `../images/91.gif`
+    }, {
+        id: 92,
+        savol: 'Harakatlanish o’ngga',
+        javob: `../images/92.gif`
+    }, {
+        id: 93,
+        savol: 'Harakatlanish chapga',
+        javob: `../images/93.gif`
+    }, {
+        id: 94,
+        savol: ` Harakatlanish to’g’riga yoki o’ngga`,
+        javob: `../images/94.gif`
+    }, {
+        id: 95,
+        savol: 'Harakatlanish to’g’riga yoki chapga',
+        javob: `../images/95.gif`
+    }, {
+        id: 96,
+        savol: 'Harakatlanish o’ngga yoki chapga',
+        javob: `../images/96.gif`
+    },
+
+    {
+        id: 97,
+        savol: 'To’siqni o’ngdan chetlab o’tish',
+        javob: `../images/97.gif`
+    }, {
+        id: 98,
+        savol: 'To’siqni chapdan chetlab o’tish',
+        javob: `../images/98.gif`
+    }, {
+        id: 99,
+        savol: 'To’siqni o’ngdan yoki chapdan chetlab o’tish',
+        javob: `../images/99.gif`
+    }, {
+        id: 100,
+        savol: 'Aylanma harakatlanish',
+        javob: `../images/100.gif`
+    }
+];
+
+let glavniy = document.querySelector('#glavniy');
+let game = document.querySelector('#game');
+let gameType = document.querySelector('#qiyin');
+let formEl = document.querySelector('#formEl');
+let btnWrapper = document.querySelector('#btnWrapper');
+let timeSelect = document.querySelector('#vaqt');
+let minut = document.querySelector('#minut');
+let sekund = document.querySelector('#sekund');
+let savolEl = document.querySelector('#savol');
+let gameOver = document.querySelector('#gameover');
+let gameOverH1 = document.querySelector('.oyin');
+let gameOverP = document.querySelector('.hulosa');
+
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+
+function timeSet() {
+    minut.innerHTML = timeSelect.value - 1;
+    sekund.innerHTML = '59';
+    setInterval(() => {
+        if (sekund.innerHTML <= 0 && minut.innerHTML <= 0) {
+            sekund.innerHTML = "00";
+            minut.innerHTML = "00";
+            sekund.style.color = 'red';
+            minut.style.color = 'red';
+            gameOver.classList.add('show');
+        } else if (minut.innerHTML >= 0 && sekund.innerHTML == 0) {
+            sekund.innerHTML = '59';
+            minut.innerHTML = `${minut.innerHTML - 1}`
+
+        } else {
+            sekund.innerHTML = `${sekund.innerHTML - 1}`;
+        }
+
+    }, 1000);
+
+
+
+
+}
+
+function logika(savolSon,b) {
+    let boxNum = [];
+    let boxSavol = [];
+    glavniy.style.display = 'none';
+    game.style.display = 'flex';
+
+
+
+    for (let i = 0; i <= savolSon; i++) {
+        let randomCardNumber = getRandomInt(90);
+        boxNum.forEach(numbs => {
+            if (randomCardNumber == numbs) {
+                randomCardNumber = randomCardNumber + 1;
+            }
+        })
+        boxNum.push(randomCardNumber);
+        boxSavol.push({
+            savol: `${savollar[randomCardNumber].savol}`,
+            id: savollar[randomCardNumber].id
+        });
+
+        let btnElements = document.createElement('button');
+        let btnElementsImg = document.createElement('img');
+        btnElementsImg.setAttribute('class', 'btn-inner');
+        btnElements.setAttribute('id', `${savollar[randomCardNumber].id}`);
+        btnElementsImg.setAttribute('src', `${savollar[randomCardNumber].javob}`);
+        btnElements.setAttribute('class', 'btn');
+
+        btnWrapper.appendChild(btnElements);
+        btnElements.appendChild(btnElementsImg);
+
+    }
+    let remdomSavol = getRandomInt(20);
+    savolEl.innerHTML = `${boxSavol[remdomSavol].savol}`;
+    let btnEl = document.querySelectorAll('.btn');
+    btnEl.forEach(elem => {
+        elem.addEventListener('click', function (e) {
+            e.preventDefault();
+            if (elem.id == boxSavol[remdomSavol].id) {
+                elem.style.opacity = '0';
+                boxSavol.splice(remdomSavol, 1);
+                b = b - 1;
+                remdomSavol = getRandomInt(b);
+                savolEl.innerHTML = `${boxSavol[remdomSavol].savol}`;
+                if (boxSavol.length == 1) {
+                    gameOverH1.innerHTML = 'YOU WIN';
+                    gameOverP.innerHTML = 'Pravani pulga olmagan ekansiz ;)';
+                    gameOver.classList.add('show');
+                }
+            } else {
+                sekund.innerHTML = sekund.innerHTML - 5;
+                elem.style.border = '10px solid red';
+                setTimeout(() => {
+                    elem.style.border = '10px solid transparent';
+                }, 3000);
+            }
+
+        })
+    })
+}
+
+formEl.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+
+    timeSet();
+
+    switch (true) {
+        case gameType.value == 'ease':
+                logika(19, 19);
+
+            break;
+            case gameType.value == 'normal':
+                logika(34, 34);
+
+            break;
+            case gameType.value == 'hard':
+                logika(34, 34);
+
+            break;
+
+
+
+
+
+    }
+})
+
+let btn = document.querySelectorAll('#btn');
+
+btn.forEach(item => {
+    item.addEventListener('click', function (e) {
+        e.preventDefault();
+
+    })
+})
